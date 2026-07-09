22b79decf77975385a9f880510fdec778196f77e
liuye09110826-crypto <liuye09110826@gmail.com>
2026-07-09 14:18:46 +0800

    update db config

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql2");
 
 // 创建连接
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "0826",
+//   database: "vue_demo"
+// });
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "0826",
-  database: "vue_demo"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // 连接数据库
